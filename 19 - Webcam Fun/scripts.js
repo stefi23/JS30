4ae9225865ae048584a30ef0efd0a6f64c65c44b
Stefi Rosca <stefania.rosca2309@gmail.com>
2021-09-03 13:37:02 +0200

    First part of Webcam project"

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,59 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+        video.srcObject = localMediaStream;
+        video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+  
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, width, height);
+      // take the pixels out
+      let pixels = ctx.getImageData(0, 0, width, height);
+      // mess with them
+      // pixels = redEffect(pixels);
+  
+    //   pixels = rgbSplit(pixels);
+      // ctx.globalAlpha = 0.8;
+  
+      // pixels = greenScreen(pixels);
+      // put them back
+    //   ctx.putImageData(pixels, 0, 0);
+    }, 16);
+  }
+
+  function takePhoto(){
+      snap.currenTime = 0
+      snap.play()
+
+      //take the data out of the canvas
+      const data = canvas.toDataURL('image/jpeg')
+      const link = document.createElement('a')
+
+      link.href = data
+      link.setAttribute('download', 'handsome')
+      link.innerHTML = `<img src=${data} alt="Handsome Person" />`
+      strip.insertBefore(link, strip.firstChild)
+
+
+  }
+  
+  getVideo();
+
+  video.addEventListener('canplay', paintToCanvas)
+  
