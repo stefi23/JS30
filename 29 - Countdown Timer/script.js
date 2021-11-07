11e227d6b779f94f01a10307088be96da28c3d5b
Stefi Rosca <stefania.rosca2309@gmail.com>
2021-11-07 21:33:49 +0200

    Add starter file and add logic part to script;

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,19 @@
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  console.log(seconds);
+}
